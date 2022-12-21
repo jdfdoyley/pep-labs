4db89bd503a0f6d85e5fbd967aff5132c03bd6c0
Jason D'Oyley <jdfd.dev@icloud.com>
2022-12-21 11:29:26 +0000

    Saved progress on Comparisons 2022-12-21 11:29:26 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -1,10 +1,10 @@
 
 /**
- * If we want to make programs that "do something", we're going to need to create instructions that change
- * depending on the current state of the program.
+ * If we want to make programs that "do something", we're going to need to create instructions that
+ * change depending on the current state of the program.
  * Therefore, we're inevitably going to run into boolean expressions (ie true/false statements).
  * Let's start with the different ways to compare two numbers, in this case a and b.
- * 
+ *
  * Additional Resource if needed: https://www.oreilly.com/library/view/java-for-dummies/9781118239742/a17.html
  */
 public class Comparators {
@@ -14,7 +14,7 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        return (a == b) ? true : false;
     }
 
     /**
@@ -23,7 +23,7 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        return (a > b) ? true : false;
     }
     /**
      * @param a the first number to be compared.
@@ -31,7 +31,7 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        return (a < b) ? true : false;
     }
     /**
      * @param a the first number to be compared.
@@ -39,7 +39,7 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        return (a >= b) ? true : false;
     }
     /**
      * @param a the first number to be compared.
@@ -47,7 +47,7 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        return (a <= b) ? true : false;
     }
 
     /**
@@ -56,6 +56,6 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        return (a != b) ? true : false;
     }
 }
