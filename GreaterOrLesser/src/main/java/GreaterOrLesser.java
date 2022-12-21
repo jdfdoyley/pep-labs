4491ad960a07e45c9c8c0fd9d218eee332c66332
Jason D'Oyley <jdfd.dev@icloud.com>
2022-12-21 15:20:17 +0000

    Saved progress on GreaterOrLesser 2022-12-21 15:20:17 +0000

--- GreaterOrLesser/src/main/java/GreaterOrLesser.java
+++ GreaterOrLesser/src/main/java/GreaterOrLesser.java
@@ -1,15 +1,22 @@
 
 public class GreaterOrLesser {
     /**
-     * Let's say part of a calculator app has to judge whether one number is bigger than another. This problem will
-     * require you to use an if/else statement.
+     * Let's say part of a calculator app has to judge whether one number is bigger than another.
+     * This problem will require you to use an if/else statement.
      *
      * @param a first number to be compared.
      * @param b second number to be compared.
-     * @return  If a is less than b, return the string "Lesser". if a is greater than b, return "Greater".
+     * @return  If a is less than b, return the string "Lesser". if a is greater than b, return
+     *          "Greater".
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
     public String decide(int a, int b){
-        return "Equal";
+        String returnValue;
+
+        if (a < b) returnValue = "Lesser";
+        else if (a > b) returnValue = "Greater";
+        else returnValue = "Equal";
+
+        return returnValue;
     }
 }
