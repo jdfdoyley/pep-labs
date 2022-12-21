8f20dd9d708a704a3dff70fe3a225c83734a3395
Jason D'Oyley <jdfd.dev@icloud.com>
2022-12-21 15:43:12 +0000

    Saved progress on Constructors 2022-12-21 15:43:12 +0000

--- Constructors/src/main/java/Player.java
+++ Constructors/src/main/java/Player.java
@@ -3,11 +3,13 @@
  * This class contains two constructors, but currently the constructors have no behavior,
  * which causes the tests to fail.
  *
- * The first constructor should take in a player's name, and set the Player object's playerName to the parameter.
- * Consider leveraging the 'this' keyword. Since a level is not provided, the constructor should also
- * set the player's level to 1 by default.
+ * The first constructor should take in a player's name, and set the Player object's playerName to
+ * the parameter.
+ * Consider leveraging the 'this' keyword. Since a level is not provided, the constructor should
+ * also set the player's level to 1 by default.
  *
- * The second constructor will use a player's name and level to set the Player object's respective field.
+ * The second constructor will use a player's name and level to set the Player object's respective
+ * field.
  *
  * These constructors would be called to instantiate the object as seen is a previous lab:
  *      Player newPlayer = new Player("Mario");
@@ -31,7 +33,8 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-
+        this.playerName = playerName;
+        this.level = 1;
     }
 
     /**
@@ -48,7 +51,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        this.playerName = playerName;
+        this.level = level;
     }
 
 }
