16c938ee81950991c0d1c993a6dd8ed2f1c37bdc
Jason D'Oyley <jdfd.dev@icloud.com>
2023-01-10 02:35:02 +0000

    Saved progress on FizzBuzz 2023-01-10 02:35:02 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,20 +1,35 @@
 
 public class FizzBuzz {
     /**
-     * Return "Fizz" if a number n is divisible by 3, "Buzz" if divisible by 5, and "FizzBuzz" if divisible by both.
+     * Return "Fizz" if a number n is divisible by 3, "Buzz" if divisible by 5, and
+     * "FizzBuzz" if divisible by both.
      *
-     * This is coding problem that will make use of the modulus (%) operator, which can help us calculate if a number
-     * is perfectly divisible by another number.
+     * This is coding problem that will make use of the modulus (%) operator, which
+     * can help us calculate if a number is perfectly divisible by another number.
      * The modulus operator returns the remainder after a division (eg 8%3=2).
      *
-     * If you try to copy answers from the internet, you will probably get this challenge wrong. The premise is
-     * slightly simpler than the classic fizzbuzz problem.
+     * If you try to copy answers from the internet, you will probably get this
+     * challenge wrong.
+     * The premise is slightly simpler than the classic fizzbuzz problem.
      *
      * @param n the number that will determine the fizzbuzz result.
-     * @return if a number is divisible by 3, return 'Fizz'. if a number is divisible by 5, return 'Buzz'. If
-     * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
+     * @return if a number is divisible by 3, return 'Fizz'. if a number is
+     *         divisible by 5, return 'Buzz'. If divisible by both, return
+     *         'FizzBuzz'. If none of these are true, return a blank string "".
      */
-    public String FizzBuzzGen(int n){
-        return "";
+    public String FizzBuzzGen(int n) {
+        String fizzBuzz = "";
+
+        if (n % 3 == 0 && !(n % 5 == 0)) {
+            fizzBuzz = "Fizz";
+        } else if ((n % 5 == 0) && !(n % 3 == 0)) {
+            fizzBuzz = "Buzz";
+        } else if ((n % 3 == 0) && (n % 5 == 0)) {
+            fizzBuzz = "FizzBuzz";
+        } else {
+            fizzBuzz = "";
+        }
+
+        return fizzBuzz;
     }
 }
