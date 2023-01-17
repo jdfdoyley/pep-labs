9a228f5c6fd896e6c1284c07a06e3aa2f8751083
Jason D'Oyley <jdfd.dev@icloud.com>
2023-01-17 01:56:19 +0000

    Saved progress on GroceryList 2023-01-17 01:56:19 +0000

--- GroceryList/src/main/java/GroceryMain.java
+++ GroceryList/src/main/java/GroceryMain.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
+
 /**
  * There is no need to change anything in this class.
  * It does not affect the outcome of the test cases.
- * You can, however, run this class to play with a CLI menu that interacts with the code you write in
+ * You can, however, run this class to play with a CLI menu that interacts with
+ * the code you write in
  * GroceryDAO.
  */
 public class GroceryMain {
@@ -16,10 +18,10 @@ public class GroceryMain {
         GroceryDAO groceryDAO = new GroceryDAO();
         databaseSetup();
         boolean active = true;
-        while(active) {
+        while (active) {
             System.out.println("What would you like to do?" +
                     "\n 1: Try to add a grocery item." +
-                    "\n 2: See all the groceries."+
+                    "\n 2: See all the groceries." +
                     "\n 3: Quit");
             Scanner inputScanner = new Scanner(System.in);
             int inputSelection = inputScanner.nextInt();
@@ -43,18 +45,20 @@ public class GroceryMain {
     }
 
     /**
-     * For the purpose of this short exercise, this method will destroy and set up a new grocery table.
-     * This is not a normal way to set up your tables, in your actual projects you should set up your database
+     * For the purpose of this short exercise, this method will destroy and set up a
+     * new grocery table.
+     * This is not a normal way to set up your tables, in your actual projects you
+     * should set up your database
      * schema in a SQL editor such as DBeaver or DataGrip.
      */
-    public static void databaseSetup(){
+    public static void databaseSetup() {
         try {
             Connection conn = ConnectionUtil.getConnection();
             PreparedStatement ps1 = conn.prepareStatement("drop table if exists grocery");
             ps1.executeUpdate();
             PreparedStatement ps2 = conn.prepareStatement("create table grocery(grocery_name varchar(255))");
             ps2.executeUpdate();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
