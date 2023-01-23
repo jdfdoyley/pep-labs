9591fab8d4d14f4d1eb713d421c50ebc87c29c0b
Jason D'Oyley <jdfd.dev@icloud.com>
2023-01-23 23:50:29 +0000

    Saved progress on StatusCodes 2023-01-23 23:50:29 +0000

--- StatusCodes/src/main/java/com/revature/JavalinSingleton.java
+++ StatusCodes/src/main/java/com/revature/JavalinSingleton.java
@@ -4,21 +4,23 @@ import io.javalin.Javalin;
 
 public class JavalinSingleton {
 
-    public static Javalin getInstance(){
+    public static Javalin getInstance() {
         Javalin app = Javalin.create();
-        
+
         /**
-         * Assignment: return the status code for a generic "client side error" in the http response
-         * 
-         * Note: Please refer to the "StatusCodes.MD" file for more assistance if needed.
+         * Assignment: return the status code for a generic "client side error" in the
+         * http response
+         *
+         * Note: Please refer to the "StatusCodes.MD" file for more assistance if
+         * needed.
          */
         app.get("/client-side-err", ctx -> {
-        
-            //write logic here
+
+            ctx.status(400);
 
         });
 
         return app;
     }
-    
+
 }
