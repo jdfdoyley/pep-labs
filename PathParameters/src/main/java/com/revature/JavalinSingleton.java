1d55aafba4a41551d8ab0f7c50c8737de130cc2c
Jason D'Oyley <jdfd.dev@icloud.com>
2023-01-24 00:02:55 +0000

    Saved progress on PathParameters 2023-01-24 00:02:55 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -4,24 +4,24 @@ import io.javalin.Javalin;
 
 public class JavalinSingleton {
 
-    public static Javalin getInstance(){
+    public static Javalin getInstance() {
         Javalin app = Javalin.create();
-        
+
         /**
-         * Assignment: retrieve the variable "first" from the path parameter and send it in the response body. Produce
-         * the response using:
-         *      ctx.result(StringVariable);
-         * 
-         * Note: Please refer to the "PathParameters.MD" file for more assistance if needed.
+         * Assignment: retrieve the variable "first" from the path parameter and send it
+         * in the response body. Produce the response using:
+         * ctx.result(StringVariable);
+         *
+         * Note: Please refer to the "PathParameters.MD" file for more assistance if
+         * needed.
          */
         app.get("/firstname/{first}", ctx -> {
-            
-            //write code here
+            String firstname = ctx.pathParam("first");
+            ctx.result(firstname);
 
         });
 
-
         return app;
     }
-    
+
 }
